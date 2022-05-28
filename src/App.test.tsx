5362519a419fa75b4392911faf8eae48d23e6258
Luke <l.beach@resilientplc.com>
2022-05-28 14:02:10 +0100

    add react router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import Demo from "./pages/demo"
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test("renders learn react link", () => {
+  render(
     <Provider store={store}>
-      <App />
+      <Demo />
     </Provider>
-  );
+  )
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+  expect(screen.getByText(/learn/i)).toBeInTheDocument()
+})
